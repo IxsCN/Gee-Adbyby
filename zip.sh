1d8c1c6a563c149d592e074f10d94eddeece93d7
天凉好个秋 <>
2016-05-05 20:11:52 +0800

    Changes to be committed: 	modified:   adbyby-gee.tar.gz 	modified:   zip.sh

--- zip.sh
+++ zip.sh
@@ -1,4 +1,11 @@
+ADBYBY_DIR="/var/etc/adbyby"
+DEV_DIR="/tmp/data/adbyby-gee"
+
+cp $ADBYBY_DIR/MAC_list $DEV_DIR/MAC_list
+cp $DEV_DIR/MAC_list.null $ADBYBY_DIR/MAC_list
 
 #开始打包
 cd /tmp/data/adbyby-gee
-tar czvf adbyby-gee.tar.gz /tmp/data/adbyby/* /etc/init.d/adbyby /etc/market/adblock.script
+tar czvf adbyby-gee.tar.gz /var/etc/adbyby/* /etc/init.d/adbyby /etc/market/adblock.script
+
+cp $DEV_DIR/MAC_list $ADBYBY_DIR/MAC_list
