fbd400aec0e1037a19f346db9371e2be0a505119
IxsCN <Ixs.CN@hotmail.com>
2016-11-26 18:09:08 +0800

    fix bug
    修复了当network重启时 adbyby可能失效的问题

--- zip.sh
+++ zip.sh
@@ -1,11 +1,7 @@
 ADBYBY_DIR="/var/etc/adbyby"
 DEV_DIR="/tmp/data/adbyby-gee"
-
-cp $ADBYBY_DIR/MAC_list $DEV_DIR/MAC_list
-cp $DEV_DIR/MAC_list.null $ADBYBY_DIR/MAC_list
+CONFIG_DIR="/etc/adbyby"
 
 #开始打包
 cd /tmp/data/adbyby-gee
-tar czvf adbyby-gee.tar.gz /var/etc/adbyby/* /etc/init.d/adbyby /etc/market/adblock.script
-
-cp $DEV_DIR/MAC_list $ADBYBY_DIR/MAC_list
+tar czvf adbyby-gee.tar.gz /var/etc/adbyby/adbyby /var/etc/adbyby/adbyby.sh /var/etc/adbyby/adhook.ini /var/etc/adbyby/cron.sh /var/etc/adbyby/firewall.sh /var/etc/adbyby/update.info /var/etc/adbyby/user.action /var/etc/adbyby/data/extra.txt /var/etc/adbyby/data/lazy.txt /var/etc/adbyby/data/user.txt /var/etc/adbyby/data/video.txt /var/etc/adbyby/doc/hidecss.js /etc/init.d/adbyby /etc/market/adblock.script /etc/firewall.d/99-adbyby.sh
